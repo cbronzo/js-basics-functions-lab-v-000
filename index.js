6e4fcbd185e20796cc0508c4d3c4853b84942ad0
Catherine Bronzo <cbronzo@mac.com>
2019-03-16 04:38:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // Code your solution in this file!
 
 function distanceFromHqInBlocks(distance) {
-  if (distance < 42 )
-  return 42-distance;
+  if (distance < 42 ) {
+    return 42-distance;
+  }
   
 }
 
